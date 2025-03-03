307433768aa7a1abeece30fd0b292794a835364c
IBV1234 <isaacbvital@gmail.com>
2025-03-03 13:44:57 -0500

    test2

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-require 'Configurations/configuration.php';
-require 'src/function.php';
-require 'Routes/routes.php';
+require_once 'Configurations/configuration.php';
+require_once 'src/function.php';
+require_once 'Routes/routes.php';
 
 routeController(Uripath());
