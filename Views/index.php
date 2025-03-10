a03b88b7451dfacb350e74337f4db072deea109c
Charlyboy90 <caxducharme@icloud.com>
2025-03-10 15:12:28 -0400

    Testing

--- Views/index.php
+++ Views/index.php
@@ -2,13 +2,22 @@
 
 require 'partials/head.php';
 require 'partials/header.php';
-
+$pdo = $instance->getPDO();
+$userModel = new UserModel($pdo);
+$user = $userModel->selectAll();
 ?>
 
         <div class="row">
             <div class="col-md-3">
                 <h1>Hello World</h1>
             </div>
+            <div class="col-md-3">
+                <?php echo htmlTag("h1", [], "Hello world with helper") ?>
+            </div>
+            <div class="col-md-3">
+                <?php echo $user[0]; ?>
+            </div>
+           
         </div>
 
 <?php require 'partials/footer.php' ?>
--- index.php
+++ index.php
@@ -4,5 +4,6 @@ require 'src/router.php';
 require 'src/functions.php';
 require 'src/configuration.php';
 require 'src/initialization.php';
+require 'src/html-helpers.php';
 
 route(urlPath());
