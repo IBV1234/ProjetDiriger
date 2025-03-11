bd35fe5cc83a478ee4232ffafac5b93ee0af9037
EmerikC <emerik294@outlook.com>
2025-03-11 15:27:34 -0400

    affichage items fini

--- Views/Partials/footer.php
+++ Views/Partials/footer.php
@@ -4,5 +4,6 @@
     <!-- Container -->
 
     <script src="/public/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
+    <script src="/public/coreui-pro-5.10.0-dist/js/coreui.bundle.min.js"></script>
 </body>
 </html>
--- Views/index.php
+++ Views/index.php
@@ -6,8 +6,119 @@ require 'partials/header.php';
 ?>
 
         <div class="row">
-            <div class="col-md-3">
-                <h1>Hello World</h1>
+            <div class="col-sm-6 col-md-4 col-xl-3 px-4 py-3">
+                <a href="/item" class="text-decoration-none text-black">
+                    <div class="ratio ratio-1x1">
+                        <div class="d-flex flex-column justify-content-between align-items-center p-4 bg-blue-fallout rounded rounded-3">
+                            <h2 class="text-white">hello world</h2>
+                            <img src="/public/images/placeholder-16x9.png" class="img-fluid" alt="placeholder">
+                            <div class="bg-dark-yellow-fallout px-2 rounded-circle" style="transform: skew(0deg, 5deg);">
+                                <div style="transform: skew(0deg, -5deg);">
+                                    <h2>50,99$</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                <div class="d-flex justify-content-center align-items-center p-2">
+                    <h3 class="pe-2">2.4</h3>
+                    <div class="pb-2" data-coreui-size="lg" data-coreui-precision="0.10" data-coreui-read-only="true" data-coreui-toggle="rating" data-coreui-value="2.4"></div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-xl-3 px-4 py-3">
+                <a href="/item" class="text-decoration-none text-black">
+                    <div class="ratio ratio-1x1">
+                        <div class="d-flex flex-column justify-content-between align-items-center p-4 bg-blue-fallout rounded rounded-3">
+                            <h2 class="text-white">hello world</h2>
+                            <img src="/public/images/placeholder-16x9.png" class="img-fluid" alt="placeholder">
+                            <div class="bg-dark-yellow-fallout px-2 rounded-circle" style="transform: skew(0deg, 5deg);">
+                                <div style="transform: skew(0deg, -5deg);">
+                                    <h2>50,99$</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                <div class="d-flex justify-content-center align-items-center p-2">
+                    <h3 class="pe-2">2.4</h3>
+                    <div class="pb-2" data-coreui-size="lg" data-coreui-precision="0.10" data-coreui-read-only="true" data-coreui-toggle="rating" data-coreui-value="2.4"></div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-xl-3 px-4 py-3">
+                <a href="/item" class="text-decoration-none text-black">
+                    <div class="ratio ratio-1x1">
+                        <div class="d-flex flex-column justify-content-between align-items-center p-4 bg-blue-fallout rounded rounded-3">
+                            <h2 class="text-white">hello world</h2>
+                            <img src="/public/images/placeholder-16x9.png" class="img-fluid" alt="placeholder">
+                            <div class="bg-dark-yellow-fallout px-2 rounded-circle" style="transform: skew(0deg, 5deg);">
+                                <div style="transform: skew(0deg, -5deg);">
+                                    <h2>50,99$</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                <div class="d-flex justify-content-center align-items-center p-2">
+                    <h3 class="pe-2">2.4</h3>
+                    <div class="pb-2" data-coreui-size="lg" data-coreui-precision="0.10" data-coreui-read-only="true" data-coreui-toggle="rating" data-coreui-value="2.4"></div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-xl-3 px-4 py-3">
+                <a href="/item" class="text-decoration-none text-black">
+                    <div class="ratio ratio-1x1">
+                        <div class="d-flex flex-column justify-content-between align-items-center p-4 bg-blue-fallout rounded rounded-3">
+                            <h2 class="text-white">hello world</h2>
+                            <img src="/public/images/placeholder-16x9.png" class="img-fluid" alt="placeholder">
+                            <div class="bg-dark-yellow-fallout px-2 rounded-circle" style="transform: skew(0deg, 5deg);">
+                                <div style="transform: skew(0deg, -5deg);">
+                                    <h2>50,99$</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                <div class="d-flex justify-content-center align-items-center p-2">
+                    <h3 class="pe-2">2.4</h3>
+                    <div class="pb-2" data-coreui-size="lg" data-coreui-precision="0.10" data-coreui-read-only="true" data-coreui-toggle="rating" data-coreui-value="2.4"></div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-xl-3 px-4 py-3">
+                <a href="/item" class="text-decoration-none text-black">
+                    <div class="ratio ratio-1x1">
+                        <div class="d-flex flex-column justify-content-between align-items-center p-4 bg-blue-fallout rounded rounded-3">
+                            <h2 class="text-white">hello world</h2>
+                            <img src="/public/images/placeholder-16x9.png" class="img-fluid" alt="placeholder">
+                            <div class="bg-dark-yellow-fallout px-2 rounded-circle" style="transform: skew(0deg, 5deg);">
+                                <div style="transform: skew(0deg, -5deg);">
+                                    <h2>50,99$</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                <div class="d-flex justify-content-center align-items-center p-2">
+                    <h3 class="pe-2">2.4</h3>
+                    <div class="pb-2" data-coreui-size="lg" data-coreui-precision="0.10" data-coreui-read-only="true" data-coreui-toggle="rating" data-coreui-value="2.4"></div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-xl-3 px-4 py-3">
+                <a href="/item" class="text-decoration-none text-black">
+                    <div class="ratio ratio-1x1">
+                        <div class="d-flex flex-column justify-content-between align-items-center p-4 bg-blue-fallout rounded rounded-3">
+                            <h2 class="text-white">hello world</h2>
+                            <img src="/public/images/placeholder-16x9.png" class="img-fluid" alt="placeholder">
+                            <div class="bg-dark-yellow-fallout px-2 rounded-circle" style="transform: skew(0deg, 5deg);">
+                                <div style="transform: skew(0deg, -5deg);">
+                                    <h2>50,99$</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                <div class="d-flex justify-content-center align-items-center p-2">
+                    <h3 class="pe-2">2.4</h3>
+                    <div class="pb-2" data-coreui-size="lg" data-coreui-precision="0.10" data-coreui-read-only="true" data-coreui-toggle="rating" data-coreui-value="2.4"></div>
+                </div>
             </div>
         </div>
 
