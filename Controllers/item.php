70ab516f1051d95d17d32dd935b32b5196b063a5
Charlyboy90 <caxducharme@icloud.com>
2025-03-19 11:35:47 -0400

    start of account, panier logic done ready to test

--- Controllers/item.php
+++ Controllers/item.php
@@ -4,6 +4,8 @@ require 'src/class/Database.php';
 require 'src/class/Item.php';
 require 'Models/ItemModel.php';
 require 'src/session.php';
+require 'Models/panier-model.php';
+require 'Models/UserModel.php';
 
 sessionStart();
 
@@ -12,16 +14,22 @@ $db = Database::getInstance(CONFIGURATIONS['database'], DB_PARAMS);
 $pdo = $db->getPDO();
 $itemModel = new ItemModel($pdo);
 $items = $itemModel->selectAll();
-$itemTest = $items[10];
-$_SESSION['Item'] = $itemTest;
+$_SESSION['item'] = $items[10];
+$userModel = new UserModel($pdo);
+$user = $userModel->getUserByEmail("monsieurtesteur@gmail.com");
+$_SESSION['user'] = $user;
 
 //get item from index.................................................
-if(!isset($_SESSION['Item']))
-    redirect("error.php");
-$item = $_SESSION['Item'];
+if(!isset($_SESSION['item']))
+    redirect("error");
+
+$item = $_SESSION['item'];
 
 //send item to cart...................................................
-if(isPost())
-    redirect("panier");
+if(isPost()){
+    $panierModel = new PanierModel($pdo);
+    $panierModel->insert($_SESSION['item']->getIdItem(), 1, $_SESSION['user']->getId());
+    //add popup showing success...
+}
 
 require 'views/item.php';       
