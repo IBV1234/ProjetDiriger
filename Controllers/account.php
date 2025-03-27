78aac8149cb4786a49fc758752e56c78beed2ac3
Charlyboy90 <caxducharme@icloud.com>
2025-03-27 10:10:34 -0400

    fixed profil

--- Controllers/account.php
+++ Controllers/account.php
@@ -10,7 +10,6 @@ sessionStart();
 $db = Database::getInstance(CONFIGURATIONS['database'], DB_PARAMS);
 $pdo = $db->getPDO();
 $userModel = new UserModel($pdo);
-
-$_SESSION['user'] = $user;
+$user = $_SESSION['user'];
 
 require 'views/account.php';
