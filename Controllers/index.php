e98942d6d5a9fe57ee879de8a4abde708902d98c
EmerikC <emerik294@outlook.com>
2025-03-28 13:05:52 -0400

    no items filter index

--- Controllers/index.php
+++ Controllers/index.php
@@ -5,13 +5,14 @@ require 'src/session.php';
 require 'src/class/Item.php';
 require 'Models/ItemModel.php';
 
-sessionStart();
 $db = Database::getInstance(CONFIGURATIONS['database'], DB_PARAMS);
 $pdo = $db->getPDO();
 
 $itemModel = new ItemModel($pdo);
 $items = $itemModel->selectActive();
 
-$_SESSION['controller'] = 'index';
+if (!$items) {
+    http_response_code(404);
+}
 
 require 'views/index.php';
