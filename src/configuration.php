d89b435b193c598d77602f8d20aaf434e24d4439
IBV1234 <isaacbvital@gmail.com>
2025-04-10 09:17:38 -0400

    update routes

--- src/configuration.php
+++ src/configuration.php
@@ -3,5 +3,16 @@
 const ROUTES = [
 
     '/' => 'index.php',
-
+    '/account' => 'account.php',
+    '/account-modify' => 'account-modify.php',
+    '/connexion' => 'connexion.php',
+    '/inscription' => 'inscription.php',
+    '/item' => 'item.php',
+    '/panier-achat' => 'panier-achat.php',
+    '/delete-item' => 'delete-item.php',
+    '/payer' => 'payer.php',
+    '/inventaire' => 'inventaire.php',
+    '/deconnexion' => 'deconnexion.php',
+    '/updateItemPanier'=>'updateItemPanier.php',
+    '/item-sac' => 'item-sac.php',
 ];
