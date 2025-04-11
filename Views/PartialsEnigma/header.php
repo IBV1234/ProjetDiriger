ece6673279c0f0da987b769ab377bfb10ee52254
sabrinahuotmilliard <sabrina.huotmilliard@hotmail.com>
2025-04-11 16:42:25 -0400

    header amen

--- Views/PartialsEnigma/header.php
+++ Views/PartialsEnigma/header.php
@@ -1,25 +1,31 @@
-<header>
-    <div class="container-head">
-        <div class="col">
-            <a href="#" style="text-decoration: none; color: black;" >
-                <div class="cercle">
-                    <div class="cercle-text-1">
-                        ?
-                    </div>
-                    <div class="cercle-text-2">
-                        Enigma
-                    </div>
+<header class="header-enigma my-2">
+    <!-- logo -->
+    <div class="col">
+        <a href="/enigma" class="link-icon" >
+            <div class="cercle">
+                <div class="cercle-text-1">
+                    ?
                 </div>
-            </a>
-        </div> <!-- Ajout de mr-3 pour une marge à droite -->
-        <div class="col cercle-text-2" style="font-size: 20px;"><?=$_SESSION['user']->getAlias();?></div>
-            <div class="col cercle-text-2" style="font-size: 30px; "> 
-                <a href="#">
-                    <img src="/public/images/bars.png" alt="bars" style="width:50px; height: 50px;" />
-
-                </a>
+                <div class="cercle-text-2">
+                    Enigma
+                </div>
             </div>
-        </div>
+        </a>
     </div>
+    <!-- alias joueur -->
+    <div class="col cercle-text-2" style="font-size: 20px;">
+        <?=$_SESSION['user']->getAlias();?>
+    </div>
+    <!-- menu -->
+    <div class="dropdown col">
+        <a href="#" class="d-block link-body-emphasis text-decoration-none " data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="/public/images/bars.png" alt="bars" style="width:50px; height: 50px;" />
+        </a>
+        <ul class="dropdown-menu text-small" style="">
+            <li><a class="dropdown-item" href="/">Knapsack</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="/deconnexion">Déconnection</a></li>
+        </ul>
+    </div> 
 </header>
-<main class="mx-3 body-enigma">
+<main class="mx-3">
