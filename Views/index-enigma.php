2798bb4596e849860e1122ed24bf829cb53ba23d
sabrinahuotmilliard <sabrina.huotmilliard@hotmail.com>
2025-04-11 18:13:00 -0400

    css debut index

--- Views/index-enigma.php
+++ Views/index-enigma.php
@@ -4,6 +4,28 @@ require 'partialsEnigma/head.php';
 require 'partialsEnigma/header.php';
 
 ?>
+<div class="container-gros-index">
+    <!-- aleatoire -->
+    <div class="container-index">
+        <p>Question aléatoire</p>
+        <img src="/public/images/dice.png" alt="des" style="width:50%; height: 50%;" >
+    </div>
 
+    <!-- difficultes -->
+    <div class="container-index">
+        <p>Question selon la difficulté</p>
+        <div>
+            <a>
+                <img src="/public/images/golden-star.png" alt="etoile-facile" style="width:90px; height: 90px;" >
+            </a>
+            <a>
+                <img src="/public/images/golden-star.png" alt="etoile-moyenne" style="width:90px; height: 90px;" >
+            </a>
+            <a>
+                <img src="/public/images/golden-star.png" alt="etoile-difficile" style="width:90px; height: 90px;" >
+            </a>
+        </div>
+    </div>
+</div>
 
 <?php require 'partialsEnigma/footer.php' ?>
