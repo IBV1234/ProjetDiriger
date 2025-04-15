dc31ffd7e1cae70cadd068cfb8e71da4c8d1cafe
IBV1234 <isaacbvital@gmail.com>
2025-04-15 10:14:47 -0400

    add-userModel-file

--- models/UserModel.php
+++ models/UserModel.php
@@ -1,39 +1,47 @@
 <?php
 
 require_once 'src/class/ModelInterface.php';
-require_once 'src/class/User.php';
+require_once 'src/class/Item.php';
 
-class UserModel implements ModelInterface
+class ItemModel implements ModelInterface
 {
+
+    // La propriété pourrait être déclarée hors constructeur
+    // private PDO $pdo
+
+    // Ici la propriété $pdo est déclarée dans le constructeur directement
     public function __construct(private PDO $pdo) {}
-    
+
     public function selectAll() : null|array {
         
-        $users = [];
+        $items = [];
 
-        try{
-            $stm = $this->pdo->prepare('SELECT idJoueur, alias, nom, prenom, courriel, estAdmin, capital, pointDeVie, dexterite, poidMax FROM joueurs');
+        try {
+            $stm = $this->pdo->prepare('CALL ItemsGetAll()');
+
             $stm->execute();
-            $data = $stm->fetchAll(PDO::FETCH_ASSOC);
 
+            $data = $stm->fetchAll(PDO::FETCH_ASSOC);
+          
             if (! empty($data)) {
 
                 foreach ($data as $row) {
-                    $users[] = new User(
-                        $row['idJoueur'],
-                        $row['alias'],
-                        $row['nom'],
-                        $row['prenom'],
-                        $row['courriel'],
-                        $row['estAdmin'],
-                        $row['capital'],
-                        $row['pointDeVie'],
-                        $row['dexterite'],
-                        $row['poidMax']
+                    $items[] = new Item(
+                        $row['idItem'],
+                        $row['nomItem'],
+                        $row['typeItem'],
+                        $row['poids'],
+                        $row['quantiteStock'],
+                        $row['prix'],
+                        $row['utilite'],
+                        $row['photo'],
+                        $row['flagDispo'],
+                        $row['descriptionItem'],
+                        $row['evaluation'] ?? 0
                     );
                 }
 
-                return $users;
+                return $items;
             }
 
             return null;
@@ -43,159 +51,132 @@ class UserModel implements ModelInterface
             throw new PDOException($e->getMessage(), $e->getCode());
 
         }
-
     }
 
-    public function selectById(int $id): null|User
-    {
+    public function selectActive() : null|array {
+        
+        $items = [];
 
         try{
-            $stm = $this->pdo->prepare('SELECT idJoueur, alias, nom, prenom, courriel, estAdmin, capital, pointDeVie, dexterite, poidMax FROM joueurs WHERE idJoueur=:id');
-          
-            $stm->bindValue(":id", $id, PDO::PARAM_INT);
+
+            // $this->pdo-> car $pdo est une propriété de l'objet
+            $stm = $this->pdo->prepare('CALL ItemsGetActive()');
 
             $stm->execute();
+    
+            $data = $stm->fetchAll(PDO::FETCH_ASSOC);
 
+            if (! empty($data)) {
+                foreach ($data as $row) {
+                    $items[] = new Item(
+                        $row['idItem'],
+                        $row['nomItem'],
+                        $row['typeItem'],
+                        $row['poids'],
+                        $row['quantiteStock'],
+                        $row['prix'],
+                        $row['utilite'],
+                        $row['photo'],
+                        $row['flagDispo'],
+                        $row['descriptionItem'],
+                        $row['evaluation'] ?? 0
+                    );
+
+                }
+
+                return $items;
+            }
+
+            return null;
+
+        } catch (PDOException $e) {
+
+            throw new PDOException($e->getMessage(), $e->getCode());
+            
+        }
+    }
+    
+    public function selectById(int $idItem): null|Item {
+        try{
+            $stm = $this->pdo->prepare('CALL ItemGetById(:idItem)');
+    
+            $stm->bindValue(":idItem", $idItem, PDO::PARAM_INT);
+            
+            $stm->execute();
+    
             $data = $stm->fetch(PDO::FETCH_ASSOC);
 
-            if (!empty($data)) {
-
-                return new User(
-                    $data['idJoueur'],
-                    $data['alias'],
-                    $data['nom'],
-                    $data['prenom'],
-                    $data['courriel'],
-                    $data['estAdmin'],
-                    $data['capital'],
-                    $data['pointDeVie'],
-                    $data['dexterite'],
-                    $data['poidMax']
-                    );
-            }
-
-            return null;
-
-        } catch (PDOException $e) {
-
-            throw new PDOException($e->getMessage(), $e->getCode());
-
-        }
-
-    }
-
-    public function getUserByEmail(string $email): null|User
-    {
-        try {
-            $user = $this->pdo->prepare('CALL chercherJoueurParCourriel(:email)');
-            $user->bindValue(":email", $email, PDO::PARAM_STR);
-            $user->execute();
-
-            $data = $user->fetch(PDO::FETCH_ASSOC);
-
-            if (!empty($data)) {
-                return new User(
-                    $data['idJoueur'],
-                    $data['alias'],
-                    $data['nom'],
-                    $data['prenom'],
-                    $email,
-                    $data['estAdmin'],
-                    $data['capital'],
-                    $data['pointDeVie'],
-                    $data['dexterite'],
-                    $data['poidMax']
+            if(! empty($data)) {
+                return new Item(
+                    $data['idItem'],
+                    $data['nomItem'],
+                    $data['typeItem'],
+                    $data['poids'],
+                    $data['quantiteStock'],
+                    $data['prix'],
+                    $data['utilite'],
+                    $data['photo'],
+                    $data['flagDispo'],
+                    $data['descriptionItem'],
+                    $data['evaluation'] ?? 0
                 );
             }
-            return null;
-   
         } catch (PDOException $e) {
-            throw new PDOException($e->getMessage(), $e->getCode());
+    
+            // throw new PDOException($e->getMessage(), $e->getCode());
+            $errorMessage = sprintf(
+                "Exception ERROR : %s | Code : %s | Message : %s | Fichier : %s | Ligne : %d\n", // formatage 
+                date('Y-m-d H:i:s'),
+                $e->getCode(),
+                $e->getMessage(),
+                $e->getFile(),
+                $e->getLine()
+              );
+
+              file_put_contents('Logs/error.txt', $errorMessage, FILE_APPEND);
+
+              redirect('Views/error.php');
+
         }  
+        return null;
     }
-  
-    public function verifyPasswordUser(string $password, string $email): bool
-    {
-        try {
-            $match = $this->pdo->prepare('SELECT verificationMotDePasse(:password, :email)');
-            $match->bindValue(":email", $email, PDO::PARAM_STR);
-            $match->bindValue(":password", $password, PDO::PARAM_STR);
-            $match->execute();
 
-            $data = $match->fetch(PDO::FETCH_NUM);
-            if($data[0] == 1) 
-                return true;
-            return false;
+    public function selectByInventory(int $idJoueur) : null|array {
+        
+        $items = [];
 
-        } catch (PDOException $e) {
-            throw new PDOException($e->getMessage(), $e->getCode());
-        }
-    }
-  
-    public function modifierPasswordUser(string $nouvPassword, user $user): bool{
         try{
-            $stm = $this->pdo->prepare('CALL modifierPasswordUser(:userId, :newPassword, @success)');
-            $stm->bindValue(":userId", $user->getId(), PDO::PARAM_INT);
-            $stm->bindValue(":newPassword", $nouvPassword, PDO::PARAM_STR);
+            $stm = $this->pdo->prepare('CALL ItemsDansInventaire(:idDuJoueur)');
+            $stm->bindValue(":idDuJoueur", $idJoueur, PDO::PARAM_INT);
             $stm->execute();
+    
+            $data = $stm->fetchAll(PDO::FETCH_ASSOC);
 
-            $result = $this->pdo->query('SELECT @success')->fetch(PDO::FETCH_NUM);
-            return $result[0] == 1;
-        } catch (PDOException $e){
-            throw new PDOException($e->getMessage(), $e->getCode());
-        }
-    }
-  
-    public function nouveauSolde(int $newSolde, int $idJoueur)
-    {
-        try {
-            $request = $this->pdo->prepare('CALL majSolde(:nouveauSolde, :idDuJoueur)');
-            $request->bindValue(":nouveauSolde", $newSolde, PDO::PARAM_INT);
-            $request->bindValue(":idDuJoueur", $idJoueur, PDO::PARAM_INT);
-            $request->execute();
+            if (! empty($data)) {
+                foreach ($data as $row) {
+                    $items[] = new Item(
+                        $row['idItem'],
+                        $row['nomItem'],
+                        $row['typeItem'],
+                        $row['poids'],
+                        $row['quantite'],
+                        $row['prix']??0,
+                        $row['utilite']??0,
+                        $row['photo'],
+                        $row['flagDispo']??0,
+                        $row['descriptionItem']??'',
+                        $row['evaluation']??0
+                    );
+                }
+                return $items;
+            }
+
+            return null;
 
         } catch (PDOException $e) {
-            throw new PDOException($e->getMessage(), $e->getCode());
-        }
-    }
-    public function nouvelleDexterite(int $newDexterite, int $idJoueur){
-        try{
-            $request = $this->pdo->prepare('CALL majDexterite(:nouvelleDexterite, :idDuJoueur)');
-            $request->bindValue(":nouvelleDexterite", $newDexterite, PDO::PARAM_INT);
-            $request->bindValue(":idDuJoueur", $idJoueur, PDO::PARAM_INT);
-            $request->execute();
-        }catch(PDOException $e){
-            throw new PDOException($e->getMessage(), $e->getCode());
-        }
-    }
 
-    public function inscriptionJoueur(array $joueur){
-        try{
-            $request = $this->pdo->prepare('CALL inscriptionJoueur(:alias, :nom, :prenom, :motdepasse, :email)');
-            $request->bindValue(':alias', $joueur['alias'], PDO::PARAM_STR);
-            $request->bindValue(':nom', $joueur['nom'], PDO::PARAM_STR);
-            $request->bindValue(':prenom', $joueur['prenom'], PDO::PARAM_STR);
-            $request->bindValue(':motdepasse', $joueur['motdepasse'], PDO::PARAM_STR);
-            $request->bindValue(':email', $joueur['courriel'], PDO::PARAM_STR);
-            $request->execute();
-        }catch(PDOException $e){
             throw new PDOException($e->getMessage(), $e->getCode());
+            
         }
-    }
-
-    public function verificationAliasExistant(string $alias){
-        try{
-            $request = $this->pdo->prepare('SELECT verificationAliasExistant(:alias)');
-            $request->bindValue(':alias', $alias, PDO::PARAM_STR);
-            $request->execute();
-            $data = $request->fetch(PDO::FETCH_NUM);
-
-            if($data[0] == 1) 
-                return true;
-            return false;
-        }catch(PDOException $e){
-            throw new PDOException($e->getMessage(), $e->getCode());
-        }
-    }
+    }    
 }
-
