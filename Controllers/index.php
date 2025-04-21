a8ae05e9b89d080364b74de11b47691b30d6f44a
EmerikC <emerik294@outlook.com>
2025-04-21 16:21:32 -0400

    can't fix routing... ima have to talk to teacher

--- Controllers/index.php
+++ Controllers/index.php
@@ -12,12 +12,15 @@ sessionStart();
 $db = Database::getInstance(CONFIGURATIONS['database'], DB_PARAMS);
 $pdo = $db->getPDO();
 
+
+
 $itemModel = new ItemModel($pdo);
 $items = $itemModel->selectActive();
 $sumPanier = null;
 if(isset($_SESSION['user'])){
     $panierModel = new PanierModel($pdo);
     $sumPanier = $panierModel->SumPanier($_SESSION['user']->getId());
+    $_SESSION['poidsSac'] = $panierModel->getPoidsSacDos($_SESSION['user']->getId());
 }
 
 if (!$items) {
